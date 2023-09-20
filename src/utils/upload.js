86f0358c5ea2f4ab936bf0a4087c204f3e7adc63
aakash <aspakash000@gmail.com>
2023-09-20 19:55:26 +0530

    initial commit

---
+++ src/utils/upload.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const upload = async (file) => {
+    const data = new FormData();
+    data.append("file",file);
+    data.append("upload_preset", "fiverr");
+
+    try{
+        const res = await axios.post("https://api.cloudinary.com/v1_1/dfzaigthm/image/upload",data);
+
+        const {url} = res.data;
+        console.log(url)
+        return url;
+    }catch(err){
+        console.log(err);
+    }
+};
+
+export default upload;
